52423e4a66a2affece24d6b025d9ab61cc464886
LDCaster <devoners9@gmail.com>
2023-09-02 21:15:21 +0800

    make: halaman-berita

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -38,9 +38,9 @@ $routes->get('/', 'BerandaController::index');
 // $routes->get('/regional-potential', 'ProfileController::RegionalPotential');
 // $routes->get('/page-gallery', 'GalleryPhotoController::page_gallery');
 // $routes->get('/page-galeri-video', 'GaleriVideoController::pageVideoGallery');
-// $routes->get('/page-news', 'BeritaController::pageNews');
+$routes->get('/halaman-berita', 'BeritaController::pageNews');
 // $routes->get('/page-produk', 'ProdukController::pageProduk');
-// $routes->get('/page-news/(:any)', 'BeritaController::pageDetailNews/$1');
+$routes->get('/halaman-berita/(:any)', 'BeritaController::pageDetailNews/$1');
 // $routes->get('/page-kontak', 'KontakController::pageKontak');
 // $routes->get('/page-structure', 'ProfileController::StructureOrganization');
 
---
+++ app/Views/landingpage/pagenews.php
@@ -0,0 +1,83 @@
+<?= $this->extend('templates_lp/main'); ?>
+
+<?= $this->section('content'); ?>
+
+<!--================Blog Area =================-->
+<section class="blog_area section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mb-5 mb-lg-0">
+                <div class="blog_left_sidebar">
+                    <?php foreach ($berita as $b) :
+                        // Konversi updated_at menjadi objek DateTime
+                        $updatedDate = new DateTime($b['updated_at']);
+
+                        // Mendapatkan tanggal dalam format 'd'
+                        $tanggal = $updatedDate->format('d');
+
+                        // Mendapatkan bulan dalam format 'M'
+                        $bulan = $updatedDate->format('M');
+
+                        // Batasan jumlah karakter yang ingin ditampilkan untuk isi berita
+                        $maxKarakter = 200;
+                        $isiBerita = $b['isi'];
+                        if (strlen($isiBerita) > $maxKarakter) {
+                            $isiBerita = substr($isiBerita, 0, $maxKarakter) . '...';
+                        }
+                    ?>
+                        <article class="blog_item">
+                            <div class="blog_item_img">
+                                <img class="card-img rounded-0" src="<?= base_url('uploads/' . $b['foto']); ?>" alt="">
+                                <a href="<?= base_url('halaman-berita/' . $b['slug']); ?>" class="blog_item_date">
+                                    <h3><?= $tanggal; ?></h3>
+                                    <p><?= $bulan; ?></p>
+                                </a>
+                            </div>
+
+                            <div class="blog_details">
+                                <a class="d-inline-block" href="<?= base_url('halaman-berita/' . $b['slug']); ?>">
+                                    <h2><?= $b['judul_berita']; ?></h2>
+                                </a>
+                                <p><?= $isiBerita; ?></p>
+                                <ul class="blog-info-link">
+                                    <li><a href="#"><i class="fa fa-user"></i><?= $b['kategori_berita']; ?></a></li>
+                                </ul>
+                            </div>
+                        </article>
+                    <?php endforeach; ?>
+                    
+                    <!-- <nav class="blog-pagination justify-content-center d-flex">
+                        <ul class="pagination">
+                            <li class="page-item">
+                                <a href="#" class="page-link" aria-label="Previous">
+                                    <i class="ti-angle-left"></i>
+                                </a>
+                            </li>
+                            <li class="page-item">
+                                <a href="#" class="page-link">1</a>
+                            </li>
+                            <li class="page-item active">
+                                <a href="#" class="page-link">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a href="#" class="page-link" aria-label="Next">
+                                    <i class="ti-angle-right"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav> -->
+
+                    
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="blog_right_sidebar">
+                    <!-- Sidebar lainnya -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================Blog Area =================-->
+
+<?= $this->endSection(); ?>
